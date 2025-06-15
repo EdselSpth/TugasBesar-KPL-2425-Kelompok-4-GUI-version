89e5437c595726408d0142560529e3e862925f9a
edselspth <edselspth@gmail.com>
2025-06-15 10:45:32 +0700

    fix: [ESH] Memperbaiki implementasi GUI

--- Views-Regreem/Menu/LoginMenu.Designer.cs
+++ Views-Regreem/Menu/LoginMenu.Designer.cs
@@ -35,6 +35,10 @@
             this.inputBox_Email = new System.Windows.Forms.TextBox();
             this.InputBox_Password = new System.Windows.Forms.TextBox();
             this.label_Text4 = new System.Windows.Forms.Label();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.label_Text5 = new System.Windows.Forms.Label();
+            this.button_ToRegister = new System.Windows.Forms.Button();
+            this.ErrorMessage = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -83,7 +87,7 @@
             // 
             this.inputBox_Email.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.inputBox_Email.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.inputBox_Email.Location = new System.Drawing.Point(297, 147);
+            this.inputBox_Email.Location = new System.Drawing.Point(298, 147);
             this.inputBox_Email.Name = "inputBox_Email";
             this.inputBox_Email.Size = new System.Drawing.Size(452, 25);
             this.inputBox_Email.TabIndex = 4;
@@ -93,31 +97,75 @@
             // 
             this.InputBox_Password.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.InputBox_Password.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.InputBox_Password.Location = new System.Drawing.Point(297, 221);
+            this.InputBox_Password.Location = new System.Drawing.Point(297, 210);
             this.InputBox_Password.Name = "InputBox_Password";
             this.InputBox_Password.Size = new System.Drawing.Size(452, 25);
             this.InputBox_Password.TabIndex = 6;
-            this.InputBox_Password.Text = "Masukkan Email Anda";
+            this.InputBox_Password.Text = "Masukkan Kata Sandi Anda";
             // 
             // label_Text4
             // 
             this.label_Text4.AutoSize = true;
             this.label_Text4.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_Text4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label_Text4.Location = new System.Drawing.Point(293, 198);
+            this.label_Text4.Location = new System.Drawing.Point(293, 187);
             this.label_Text4.Name = "label_Text4";
-            this.label_Text4.Size = new System.Drawing.Size(47, 20);
+            this.label_Text4.Size = new System.Drawing.Size(85, 20);
             this.label_Text4.TabIndex = 5;
-            this.label_Text4.Text = "Email";
+            this.label_Text4.Text = "Kata Sandi";
+            // 
+            // button_Login
+            // 
+            this.button_Login.BackColor = System.Drawing.Color.SeaGreen;
+            this.button_Login.ForeColor = System.Drawing.Color.Transparent;
+            this.button_Login.Location = new System.Drawing.Point(297, 251);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(176, 45);
+            this.button_Login.TabIndex = 9;
+            this.button_Login.Text = "Masuk";
+            this.button_Login.UseVisualStyleBackColor = false;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // label_Text5
+            // 
+            this.label_Text5.AutoSize = true;
+            this.label_Text5.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Text5.Location = new System.Drawing.Point(294, 321);
+            this.label_Text5.Name = "label_Text5";
+            this.label_Text5.Size = new System.Drawing.Size(143, 19);
+            this.label_Text5.TabIndex = 10;
+            this.label_Text5.Text = "Belum Punya Akun???";
+            // 
+            // button_ToRegister
+            // 
+            this.button_ToRegister.Location = new System.Drawing.Point(443, 317);
+            this.button_ToRegister.Name = "button_ToRegister";
+            this.button_ToRegister.Size = new System.Drawing.Size(154, 28);
+            this.button_ToRegister.TabIndex = 11;
+            this.button_ToRegister.Text = "Buat Sekarang";
+            this.button_ToRegister.UseVisualStyleBackColor = true;
+            this.button_ToRegister.Click += new System.EventHandler(this.button_ToRegister_Click);
+            // 
+            // ErrorMessage
+            // 
+            this.ErrorMessage.AutoSize = true;
+            this.ErrorMessage.Location = new System.Drawing.Point(295, 362);
+            this.ErrorMessage.Name = "ErrorMessage";
+            this.ErrorMessage.Size = new System.Drawing.Size(0, 16);
+            this.ErrorMessage.TabIndex = 12;
             // 
             // Regreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ErrorMessage);
+            this.Controls.Add(this.button_ToRegister);
+            this.Controls.Add(this.label_Text5);
             this.Controls.Add(this.InputBox_Password);
-            this.Controls.Add(this.label_Text4);
             this.Controls.Add(this.inputBox_Email);
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.label_Text4);
             this.Controls.Add(this.label_Text3);
             this.Controls.Add(this.label_Text2);
             this.Controls.Add(this.label_text1);
@@ -139,6 +187,10 @@
         private System.Windows.Forms.TextBox inputBox_Email;
         private System.Windows.Forms.TextBox InputBox_Password;
         private System.Windows.Forms.Label label_Text4;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Label label_Text5;
+        private System.Windows.Forms.Button button_ToRegister;
+        private System.Windows.Forms.Label ErrorMessage;
     }
 }
 
