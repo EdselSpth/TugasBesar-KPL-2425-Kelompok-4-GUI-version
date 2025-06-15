63b58a1d2a93d08b79974a76743fc0ddb86844bb
Rd27p <g.a.raka.darma.p.k@gmail,com>
2025-06-15 15:25:11 +0700

    fix: [GAR] peerbaikan pada dashboard admin (15 Juni 20250

--- View-Regreen/Admin/DashboardAdmin.Designer.cs
+++ View-Regreen/Admin/DashboardAdmin.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class DashboardAdmin
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,12 +15,9 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             panel_1 = new Panel();
             pictureBox6 = new PictureBox();
             pictureBox5 = new PictureBox();
@@ -35,6 +25,13 @@
             pictureBox3 = new PictureBox();
             Beranda = new PictureBox();
             pictureBox1 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            bindingSource1 = new BindingSource(components);
+            label2 = new Label();
+            dateTimePickerFilter = new DateTimePicker();
+            btnFilterTanggal = new Button();
+
             panel_1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
@@ -42,10 +39,11 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)Beranda).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
             SuspendLayout();
-            // 
+
             // panel_1
-            // 
             panel_1.BackColor = SystemColors.ButtonHighlight;
             panel_1.Controls.Add(pictureBox6);
             panel_1.Controls.Add(pictureBox5);
@@ -55,77 +53,111 @@
             panel_1.Controls.Add(pictureBox1);
             panel_1.Location = new Point(0, 0);
             panel_1.Name = "panel_1";
-            panel_1.Size = new Size(215, 453);
+            panel_1.Size = new Size(215, 673);
             panel_1.TabIndex = 0;
-            // 
+
             // pictureBox6
-            // 
             pictureBox6.Image = Properties.Resources.Keluar;
-            pictureBox6.Location = new Point(18, 399);
+            pictureBox6.Location = new Point(16, 625);
             pictureBox6.Name = "pictureBox6";
             pictureBox6.Size = new Size(101, 27);
             pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox6.TabIndex = 5;
             pictureBox6.TabStop = false;
-            // 
+
             // pictureBox5
-            // 
             pictureBox5.Image = Properties.Resources.Penarikan_Menu_OFF;
             pictureBox5.Location = new Point(16, 275);
             pictureBox5.Name = "pictureBox5";
             pictureBox5.Size = new Size(185, 48);
             pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox5.TabIndex = 4;
             pictureBox5.TabStop = false;
-            // 
+
             // pictureBox4
-            // 
             pictureBox4.Image = Properties.Resources.Area_Menu_OFF;
             pictureBox4.Location = new Point(16, 221);
             pictureBox4.Name = "pictureBox4";
             pictureBox4.Size = new Size(185, 48);
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox4.TabIndex = 3;
             pictureBox4.TabStop = false;
-            // 
+
             // pictureBox3
-            // 
             pictureBox3.Image = Properties.Resources.Penjadwalan_Menu_OFF1;
             pictureBox3.Location = new Point(16, 167);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(185, 48);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox3.TabIndex = 2;
             pictureBox3.TabStop = false;
-            // 
+
             // Beranda
-            // 
             Beranda.Image = Properties.Resources.Beranda_Menu;
             Beranda.Location = new Point(16, 113);
             Beranda.Name = "Beranda";
             Beranda.Size = new Size(185, 48);
             Beranda.SizeMode = PictureBoxSizeMode.Zoom;
-            Beranda.TabIndex = 1;
             Beranda.TabStop = false;
-            // 
+
             // pictureBox1
-            // 
             pictureBox1.Image = Properties.Resources.Logo_Regreen;
             pictureBox1.Location = new Point(18, 18);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(183, 67);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
-            // 
+
+            // dataGridView1
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(240, 125);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(995, 367);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+
+            // label1
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(240, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(336, 31);
+            label1.Text = "Jadwal Pengambilan Terdaftar";
+
+            // label2
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(240, 521);
+            label2.Name = "label2";
+            label2.Size = new Size(202, 31);
+            label2.Text = "Cari Pengambilan";
+
+            // dateTimePickerFilter
+            dateTimePickerFilter.Location = new Point(240, 567);
+            dateTimePickerFilter.Name = "dateTimePickerFilter";
+            dateTimePickerFilter.Size = new Size(263, 27);
+
+            // btnFilterTanggal
+            btnFilterTanggal.Location = new Point(240, 616);
+            btnFilterTanggal.Name = "btnFilterTanggal";
+            btnFilterTanggal.Size = new Size(204, 33);
+            btnFilterTanggal.Text = "Cari";
+            btnFilterTanggal.UseVisualStyleBackColor = true;
+            btnFilterTanggal.Click += btnFilterTanggal_Click;
+
             // DashboardAdmin
-            // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1262, 673);
+            Controls.Add(btnFilterTanggal);
+            Controls.Add(dateTimePickerFilter);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
             Controls.Add(panel_1);
             Name = "DashboardAdmin";
-            Text = "Form1";
+            Text = "Dashboard Admin";
+            Load += DashboardAdmin_Load;
+
             panel_1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
@@ -133,7 +165,10 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)Beranda).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -145,5 +180,11 @@
         private PictureBox pictureBox3;
         private PictureBox Beranda;
         private PictureBox pictureBox6;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private BindingSource bindingSource1;
+        private Label label2;
+        private DateTimePicker dateTimePickerFilter;
+        private Button btnFilterTanggal;
     }
 }
--- View-Regreen/Admin/DashboardAdmin.cs
+++ View-Regreen/Admin/DashboardAdmin.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,11 +9,73 @@ namespace View_Regreen.Menu
 {
     public partial class DashboardAdmin : Form
     {
+        private readonly HttpClient _httpClient = new();
+        private List<JadwalModel> _allJadwals = new();
+
         public DashboardAdmin()
         {
             InitializeComponent();
             this.BackColor = ColorTranslator.FromHtml("#E8EDDE");
             panel_1.BackColor = ColorTranslator.FromHtml("#D6E6C4");
         }
+
+        private async void DashboardAdmin_Load(object sender, EventArgs e)
+        {
+            await LoadAllJadwalAsync();
+        }
+
+        private async Task LoadAllJadwalAsync()
+        {
+            try
+            {
+                string url = "https://localhost:7277/api/jadwal_admin";
+                var response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var jsonString = await response.Content.ReadAsStringAsync();
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var jadwals = JsonSerializer.Deserialize<List<JadwalModel>>(jsonString, options);
+
+                if (jadwals != null)
+                {
+                    _allJadwals = jadwals;
+                    dataGridView1.DataSource = _allJadwals;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal memuat data: " + ex.Message);
+            }
+        }
+
+        private void btnFilterTanggal_Click(object sender, EventArgs e)
+        {
+            var selectedDate = dateTimePickerFilter.Value.Date;
+            var filteredJadwals = _allJadwals.FindAll(j => j.tanggal.Date == selectedDate);
+
+            if (filteredJadwals.Count == 0)
+            {
+                MessageBox.Show("Tidak ada jadwal pada tanggal tersebut.");
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = filteredJadwals;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+    }
+
+    public class JadwalModel
+    {
+        public DateTime tanggal { get; set; }
+        public List<string> jenisSampah { get; set; }
+        public string namaKurir { get; set; }
+        public string areaDiambil { get; set; }
+        public string hari { get; set; }
     }
 }
